d1984a359207c7728d7c33ef61d73c7236518811
Aman Kumar <dash71191@gmail.com>
2024-05-01 11:56:03 +0100

    ok

--- myprojects/first.py
+++ myprojects/first.py
@@ -6,8 +6,9 @@ app = Flask(__name__)
 def home(name):
     return render_template("index.html",webname = name,r="Hello")
 
-@app.route("/<name>")
-def home(name):
-    return render_template("index.html",webname = name,r="Hello")
+@app.route("/test")
+def test():
+    return render_template("test.html",content = ["aman","manan","naman"])
+
 if __name__ == "__main__":
     app.run()
