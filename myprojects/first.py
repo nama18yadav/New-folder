ed22d785b29a1fd3b9c87a0a71b20194407733b2
Nama8178 <yadavmayank8178@gmail.com>
2024-05-01 22:22:25 +0530

    nhi kar ra kaam

--- myprojects/first.py
+++ myprojects/first.py
@@ -2,13 +2,13 @@ from flask import Flask, redirect , url_for, render_template
 
 app = Flask(__name__)
 
-@app.route("/<name>")
+@app.route("/home")
 def home(name):
-    return render_template("index.html",webname = name,r="Hello")
+    return render_template("index.html")
 
 @app.route("/test")
 def test():
     return render_template("test.html",content = ["aman","manan","naman"])
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
